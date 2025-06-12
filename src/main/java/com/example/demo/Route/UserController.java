5e4297fbed66f4db81ad61db671611f93f7dd473
saymynamenow <muhammadfiqriananda@student.telkomuniversity.ac.id>
2025-06-12 17:27:36 +0700

    Update

--- src/main/java/com/example/demo/Route/UserController.java
+++ src/main/java/com/example/demo/Route/UserController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +30,7 @@ public class UserController {
 	public ResponseEntity<?> createUser(@RequestBody User user) {
 		Boolean userExist = userRepo.existsByUsername(user.getUsername());
 		if(userExist) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username sudah di pakai");
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Username sudah di pakai");
 		}else {
 			User userData = userRepo.save(user);
 			return ResponseEntity.ok(userData);
@@ -40,7 +42,7 @@ public class UserController {
 	public ResponseEntity<?> loginUser(@RequestBody LoginDTO logDTO){
 		Optional<User> user = userRepo.findByUsername(logDTO.getUsername());
 		if(user.isPresent()) {
-			if(user.get().getPassword().equals(logDTO.getPassword())) {				
+			if(user.get().getPassword().equals(logDTO.getPassword())) {
 				return ResponseEntity.ok(user.get());
 			}
 			
@@ -49,5 +51,24 @@ public class UserController {
 		}else {			
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username not found");
 		}
+	}
+	
+	@GetMapping("/getprofile/{id}")
+	public ResponseEntity<?> getProfile(@PathVariable Long id) {
+		Optional<User> user = userRepo.findById(id);
+		if(user.isPresent()) {
+			return ResponseEntity.ok(user.get());
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+		}
+	}
+	
+	@GetMapping("/getuser")
+	public ResponseEntity<?> getUserList() {
+		var users = userRepo.findAll();
+		if(users.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No users found");
+		}
+		return ResponseEntity.ok(users);
 	}
 }
